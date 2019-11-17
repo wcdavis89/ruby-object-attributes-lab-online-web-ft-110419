84dc9c4553534429d6e0af1a597ec4f69a6b7a84
Whittney Davis <wcdavis89@yahoo.com>
2019-11-17 19:54:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,4 +11,25 @@ class Person
   def name 
     "#{@first_name} #{@last_name}".strip 
   end
+  def job=(string)
+    @job = string
+  end
+  def job
+    @job
+  end
+end
+
+class Dog 
+  def name=(string)
+    @name = string
+  end
+  def name
+  @name
+  end
+  def breed=(string)
+    @breed = string
+  end
+  def breed
+    @breed
+  end
 end
