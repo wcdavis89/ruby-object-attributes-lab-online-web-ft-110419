f0ec914e5290179519b5fb8cae6f1ae0cbd6bba2
Whittney Davis <wcdavis89@yahoo.com>
2019-11-17 19:56:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Person
+  def name=(string)
+    @name = string
+  end
+  def name 
+    @name
+  end
+  def job=(string)
+    @job = string
+  end
+  def job
+    @job
+  end
+end
+
+class Dog 
+  def name=(string)
+    @name = string
+  end
+  def name
+  @name
+  end
+  def breed=(string)
+    @breed = string
+  end
+  def breed
+    @breed
+  end
+end
